46b185ac1d17a3acd4dd831445d3d9bbb9054f14
Corey Miller <coreymiller353@gmail.com>
2019-06-02 12:52:36 -0400

    completes censor function

--- App Academy/Projects/rspec_exercise_2/lib/part_1.rb
+++ App Academy/Projects/rspec_exercise_2/lib/part_1.rb
@@ -13,6 +13,38 @@ end
 
 
 def merge(hash1,hash2)
-    hash3 = hash1.merge(hash2)
-    return hash3
+    hash3 = {}
+    hash1.each { |k,v| hash3[k] = v}
+    hash2.each { |k,v| hash3[k] = v}
+    hash3
+end
+
+def censor(sentence,curse)
+    new_sentence = []
+    words = sentence.split(" ")
+    words.map do |word|
+        if curse.include?(word.downcase)
+            new_sentence << star(word)
+        else
+            new_sentence << word
+        end
+    end
+    return new_sentence.join(" ")
+end
+
+def star(word)
+new_word = ""
+    vowels = "aeiou"
+    word.each_char do |char|
+        if vowels.include?(char.downcase)
+            new_word += "*"
+        else
+            new_word += char
+        end
+    end
+    return new_word
+end
+
+def power_of_two?
+
 end
