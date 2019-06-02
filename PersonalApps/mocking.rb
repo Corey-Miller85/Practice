a236a68da11dabca0b936bdbc72b5420063d2f10
Corey Miller <coreymiller353@gmail.com>
2019-06-02 10:32:38 -0400

    add personal App folder and compress rspec exercise for upload

---
+++ PersonalApps/mocking.rb
@@ -0,0 +1,18 @@
+def mocking(string)
+    
+    new_string = ""
+    string.each_char.with_index do |char,i|
+            if i % 2 == 0
+                char.downcase!
+                new_string += char
+            end
+            if i % 2 !=0 
+                char.upcase!
+                new_string += char
+            end
+        end
+    end
+    return new_string
+end
+
+p mocking("all children are special")
