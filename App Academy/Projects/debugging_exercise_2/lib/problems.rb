62ca35bd43cc52dc93ef3867e4d9c59ef961f952
Corey Miller <coreymiller353@gmail.com>
2019-06-15 09:55:18 -0400

    solves largest_prime_factor

--- App Academy/Projects/debugging_exercise_2/lib/problems.rb
+++ App Academy/Projects/debugging_exercise_2/lib/problems.rb
@@ -3,12 +3,28 @@
 # Feel free to use the debugger when you get stuck.
 
 def largest_prime_factor(num)
-
+    divisors = []
+    prime_divisors = []
+    (1..num).each do |i|
+        if num % i == 0
+            divisors << i 
+        end
+        divisors.each do |ele|
+            if is_prime?(ele) == true
+                prime_divisors << ele
+            end
+        end
+    end
+    prime_divisors[-1]
 end
 
+
 def is_prime?(num)
+   return false if num < 2 
     (2...num).each do |ele| 
         return false if num % ele == 0 
     end
 return true 
 end
+
+
