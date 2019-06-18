cd7fb7232e0486ad7e7fbf43c4f1b17e63eb2ab5
Corey Miller <coreymiller353@gmail.com>
2019-06-18 12:05:11 -0400

    completes proc project part_2.rb

--- App Academy/Projects/procs_project/lib/part_2.rb
+++ App Academy/Projects/procs_project/lib/part_2.rb
@@ -27,8 +27,35 @@ end
 
 def greater_proc_value(num, proc_1, proc_2)
 
-proc_1.call(num)
+ans1 = proc_1.call(num)
+ans2 = proc_2.call(num)
+
+    if ans1 >= ans2 
+        ans1
+    else
+        ans2
+    end
 end
 
 
-p greater_proc_value(3, times_10, square)
+def and_selector(arr, prc1, prc2)
+   new_arr = []
+    arr.each do |ele|
+        if prc1.call(ele) == true && prc2.call(ele) == true
+            new_arr << ele
+        end
+    end
+    new_arr    
+end
+
+def alternating_mapper(arr, prc1, prc2)
+new_arr = []
+arr.each_with_index do |ele, i|
+        if i % 2 == 0 
+            new_arr << prc1.call(ele)
+        else
+            new_arr << prc2.call(ele)
+        end
+    end
+    return new_arr
+end
