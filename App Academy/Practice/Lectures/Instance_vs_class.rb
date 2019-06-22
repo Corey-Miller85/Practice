5a74fc65730227264c5ec47d8ff07fa789a18f9f
Corey Miller <coreymiller353@gmail.com>
2019-06-22 15:29:44 -0400

    completes dog project

--- App Academy/Practice/Lectures/Instance_vs_class.rb
+++ App Academy/Practice/Lectures/Instance_vs_class.rb
@@ -2,9 +2,13 @@ class Car
     def initialize(color)
         @color = color
     end
+
+    def get_color
+        @color
+    end
 end
 
 car_1 = Car.new("Black")
 car_2 = Car.new("Red")
 
-p car_1 
+p car_1.get_color
