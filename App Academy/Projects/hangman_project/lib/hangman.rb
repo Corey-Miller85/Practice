440b9170bd34cc856aceb49e8b0f85858a58b9ee
Corey Miller <coreymiller353@gmail.com>
2019-06-29 16:43:14 -0400

    starts solving hangman project

--- App Academy/Projects/hangman_project/lib/hangman.rb
+++ App Academy/Projects/hangman_project/lib/hangman.rb
@@ -1,4 +1,17 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
+  def self.random_word
+    DICTIONARY.sample
+  end
+    
+  def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+    
+  def guess_word
+  end
 end
