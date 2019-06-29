3ad413fe76cf32feb5b5d1b4fad733f4a886c53b
Corey Miller <coreymiller353@gmail.com>
2019-06-29 11:19:25 -0400

    completes guessing_game project

--- App Academy/Projects/guessing_game_project/lib/guessing_game.rb
+++ App Academy/Projects/guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,36 @@
 class GuessingGame
+    def initialize(num1, num2)
+        @secret_num = rand(num1..num2)
+        @num_attempts = 0
+        @game_over = false
+    end
+
+    def num_attempts
+        p @num_attempts
+    end
+
+    def game_over?
+        @game_over
+    end
+
+    def check_num(number)
+        @num_attempts += 1
+        if number == @secret_num
+            @game_over = true
+            puts "you win"
+        end
+        if number > @secret_num
+            puts "too big"
+        end
+        if number < @secret_num
+            puts "too small"
+        end
+    end
+
+    def ask_user
+        p "enter a number"
+        input = gets.chomp.to_i
+        self.check_num(input)
+    end
 
 end
