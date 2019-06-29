373931ff03faa271934fe2fa53916f76fdc5f461
Corey Miller <coreymiller353@gmail.com>
2019-06-29 11:02:33 -0400

    completes input_output lecture

--- App Academy/Practice/Lectures/input_output.rb
+++ App Academy/Practice/Lectures/input_output.rb
@@ -1,2 +1,20 @@
 puts "Enter your name: "
-gets
+username = gets
+puts "Hello, " + username
+
+puts "Enter 'yes' or 'no'" 
+response = gets.chomp
+
+
+p response
+# this will get the new line character /n .chomp will delete the \n when you have a return
+
+if response  == "yes"
+    puts "you said yes!"
+end
+
+# when you do gets you will return a string. You need to convert to interiger when working with njumbsr. you need to use ".to_i"
+
+num = gets.chomp.to_i
+
+puts 2 * num
