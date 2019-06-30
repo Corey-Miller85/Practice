69bc1cae6e611ced282d5c67e8355715e5dfb7af
Corey Miller <coreymiller353@gmail.com>
2019-06-30 10:35:37 -0400

    completes hangman project

--- App Academy/Projects/hangman_project/lib/hangman.rb
+++ App Academy/Projects/hangman_project/lib/hangman.rb
@@ -25,11 +25,7 @@ class Hangman
   end
 
   def already_attempted?(char)
-    if @attempted_chars.include?(char)
-      true
-    else
-      false
-    end
+    @attempted_chars.include?(char)
   end
 
   def get_matching_indices(char)
@@ -46,5 +42,57 @@ class Hangman
     array.each do |num|
       @guess_word[num] = char
     end
+  end
+
+  def try_guess(char)
+    
+    if self.get_matching_indices(char) == []
+      @remaining_incorrect_guesses -= 1
+    end
+    self.fill_indices(char, self.get_matching_indices(char))
+    if self.already_attempted?(char) == true
+      print "that has already been attempted"
+      return false
+    else 
+      @attempted_chars << char
+      return true
+    end 
+  end
+
+  def ask_user_for_guess
+    print "Enter a char:"
+    input = gets.chomp
+    self.try_guess(input)
+  end
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      puts "WIN"
+      return true
+    else
+      return false
+    end
+  end
+
+  def lose?
+     if @remaining_incorrect_guesses > 0 
+      return false
+    end
+    if @remaining_incorrect_guesses == 0 
+      puts "LOSE"
+      return true
+    end
+  end
+
+  def game_over?
+    if self.win? == true
+      puts @secret_word
+      return true
+    end
+    if self.lose? == true
+       puts @secret_word
+      return true
+    end
+    return false
   end
 end
