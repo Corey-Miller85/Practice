db51abba30a11fbc7f739d0514d4cb0a47131e4e
Corey Miller <coreymiller353@gmail.com>
2019-07-01 10:44:05 -0400

    begins master_mind project.

--- App Academy/ObjectOrientedProgramming/mastermind_project/lib/code.rb
+++ App Academy/ObjectOrientedProgramming/mastermind_project/lib/code.rb
@@ -1,4 +1,5 @@
 class Code
+  attr_reader :pegs
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
@@ -6,4 +7,27 @@ class Code
     "Y" => :yellow
   }
 
+    def self.valid_pegs?(chars)
+      #all chars of the array are in ::POSSIBLE_PEGS
+      chars.all? { |char| POSSIBLE_PEGS.has_key?(char.upcase) }
+    end
+    
+    def self.random(length)
+      random_pegs = []
+      length.times { random_pegs << POSSIBLE_PEGS.keys.sample}
+      Code.new(random_pegs)
+    end
+
+    def self.from_string
+
+
+    def initialize(chars)
+      if Code.valid_pegs?(chars) #are pegs valid?
+        @pegs = chars.map(&:upcase) # make pegs array and upcase
+      else
+        raise "Invalid pegs" #if the pegs are not all valid.
+      end
+    end
+
+    
 end
