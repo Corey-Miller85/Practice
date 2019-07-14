006839d239a70d1b5b788cebe8bc5c313f799379
Corey Miller <coreymiller353@gmail.com>
2019-07-14 09:29:24 -0400

    complete ticket.rb

--- WellRoundedRubyist/ticket.rb
+++ WellRoundedRubyist/ticket.rb
@@ -1,7 +1,34 @@
+ticket = Object.new
+
+def ticket.date
+    "01/02/03"
+end
+
+def ticket.venue
+    "Town Hall"
+end
+
+def ticket.event
+    "Author's reading"
+end
+
+def ticket.performer
+    "Mark Twain"
+end
+
+def ticket.seat
+    "Second Balcony, Row J, seat 12"
+end
+
+def ticket.price
+    5.50
+end
+
+
 print "This ticket is for: "
 print ticket.event + ", at "
-print ticket.venue
-puts ticket.date
+print ticket.venue + ", on "
+puts ticket.date + "."
 print "The performer is "
 puts ticket.performer + "."
 print "The seat is "
